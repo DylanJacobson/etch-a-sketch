ff5e969436348eaaec0ade68fc33ecf8f1e14147
Dylan Jacobson <dylanrobertjacobson@gmail.com>
2020-11-21 12:43:54 -0700

    Adds CSS and erase functionality

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
 let gridSize = 16;
+let mode = "fill";
 
-const sketchContainer = document.getElementById("sketch-container");
-
-sketchContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-sketchContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+const sketchContent = document.getElementById("sketch-content");
 
 const clearButton = document.getElementById("clear");
-
 clearButton.addEventListener('click', function() {
   clearGrid();
 });
 
 const resizeButton = document.getElementById("resize");
-
 resizeButton.addEventListener('click', function() {
   let newSize = prompt("What should the new grid size be?");
   gridSize = newSize;
 
-  while (sketchContainer.firstChild) {
-    sketchContainer.removeChild(sketchContainer.firstChild);
+  while (sketchContent.firstChild) {
+    sketchContent.removeChild(sketchContent.firstChild);
   }
 
   populateGrid();
 })
 
+const eraseButton = document.getElementById("erase");
+eraseButton.addEventListener('click', function() {
+  if (mode === "fill") {
+    mode = "erase";
+    eraseButton.textContent = "Fill";
+  } else if (mode === "erase") {
+    mode = "fill";
+    eraseButton.textContent = "Erase";
+  }
+});
+
 function populateGrid() {
-  sketchContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-  sketchContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  sketchContent.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  sketchContent.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
   for (i = 0; i < Math.pow(gridSize, 2); i++) {
     const newCell = document.createElement('div');
     newCell.classList.add('cell');
-    newCell.addEventListener('mouseover', function() {
-      this.classList.add('fill');
-    });
-    sketchContainer.appendChild(newCell);
+    newCell.addEventListener('mouseover', modifyCell);
+    sketchContent.appendChild(newCell);
   }
 }
 
@@ -43,5 +48,15 @@ function clearGrid() {
     cell.classList.remove('fill');
   });
 };
+
+function modifyCell() {
+  if (mode === "fill") {
+    this.classList.add('fill');
+  } else if (mode === "erase") {
+    if (this.classList.contains('fill')) {
+      this.classList.remove('fill');
+    }
+  }
+}
 
 populateGrid();
