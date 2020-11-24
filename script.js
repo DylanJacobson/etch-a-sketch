7a31c63ebb08d3f6615a1f5171dc6cc18ec71de8
Dylan Jacobson <dylanrobertjacobson@gmail.com>
2020-11-24 15:56:08 -0700

    Adds grayscale mode

--- script.js
+++ script.js
@@ -20,18 +20,23 @@ resizeButton.addEventListener('click', function() {
   while (newSize > 100 || newSize < 0 || isNaN(newSize)) {
     newSize = prompt('Make sure you enter an integer between 1 and 100!');
   }
+  
+  // this if statement prevents the sketch from disappearing if the user
+  // cancels out the newSize prompt
+  if (newSize) {
+    gridSize = newSize;
 
-  gridSize = newSize;
+    while (sketchContent.firstChild) {
+      sketchContent.removeChild(sketchContent.firstChild);
+    }
 
-  while (sketchContent.firstChild) {
-    sketchContent.removeChild(sketchContent.firstChild);
+    populateGrid();
   }
-
-  populateGrid();
 })
 
 const modeSelector = document.getElementById('modeSelector');
 modeSelector.onchange = function() {
+  clearGrid();
   mode = modeSelector.value;
 }
 
@@ -46,8 +51,8 @@ function populateGrid() {
     newCell.addEventListener('animationend', function() {
       this.classList.remove('fadeOut', 'fill');
       // removing the background color like this allows this
-      // function to work on rainbow cells
-      this.style.backgroundColor = undefined;
+      // function to work on rainbow/grayscale cells
+      this.style.backgroundColor = '';
     });
     sketchContent.appendChild(newCell);
   }
@@ -64,13 +69,11 @@ function clearGrid() {
 function modifyCell() {
   if (mode === 'fill') {
     this.classList.add('fill');
+  } else if (mode ==='grayscale') {
+      let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
+      this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
   } else if (mode === 'rainbow') {
     this.style.backgroundColor = Math.floor(Math.random()*16777215).toString(16);
-  } else if (mode === 'erase') {
-    if (this.classList.contains('fill')) {
-      this.classList.remove('fill');
-    }
-    this.style.backgroundColor = null;
   }
 }
 
